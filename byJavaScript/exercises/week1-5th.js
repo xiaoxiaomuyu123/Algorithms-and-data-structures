2d116869b79bfadb7bdbcac15d89ec0ca6f4bd75
xiaoxiaomuyu123 <bandaridreams@163.com>
2019-08-08 11:45:43 +0800

    拍平一个数组

--- byJavaScript/exercises/week1-5th.js
+++ byJavaScript/exercises/week1-5th.js
@@ -9,17 +9,33 @@ falt([',',[''], ',']) // [',', '', ',']
 
 */
 
-function flat(arr) {
-    let arr1 = [];
-    for (let i = 0; i < arr.length; i++) {
-        if(typeof arr[i] === "object") {
-            arr1.push(...flat(arr[i]))
-        } else {
-            arr1.push(arr[i]);
-        }
-    }
-    return arr1;
-}
+/************* 方法一：递归加 push 实现  **************/
+// function flat(arr) {
+//     let arr1 = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if(typeof arr[i] === "object") {
+//             arr1.push(...flat(arr[i]))
+//         } else {
+//             arr1.push(arr[i]);
+//         }
+//     }
+//     return arr1;
+// }
+
+// 用递归和 数组的 concat() 方法
+// function flat(arr) {
+//    let arr1 = [];
+//    arr.forEach(item => {
+//        if(item instanceof Array) {
+//            arr1 = arr1.concat(flat(item))
+//        } else {
+//            arr1.push(item);
+//        }
+//    })
+//     return arr1;
+// }
+
+
 
 console.log(flat([1,[2,'a'],8])) // [1,2,'a',5,6,8]
 console.log(flat([1,[2,'a',[5,6]],8])) // [1,2,'a',5,6,8]
