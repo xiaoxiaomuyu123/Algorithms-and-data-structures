5a3d636cb050263959eb654c1dea0d1aa584f724
xiaoxiaomuyu123 <bandaridreams@163.com>
2019-12-08 20:56:25 +0800

    1

--- byJavaScript/codewars/8kyu/001_数组中数字求和.js
+++ byJavaScript/codewars/8kyu/001_数组中数字求和.js
@@ -16,3 +16,12 @@ function sumMix(x){
     let sum = x.map(items => items * 1).reduce((pre, cur) => pre + cur);
     return sum;
 }
+
+/*
+用到的知识点：
+1. 用乘性操作符将字符串转换成数字，相当于使用 Number() 函数进行转换。
+2. Number（）和 parseInt（）在字符串转换的区别是：
+    * “” 空字符串，Number（）转成 0， parseInt（）转成 NaN
+    * “123adf”，number（）转成 NaN，parseInt（）转成 123
+    * number（） 将 true，false，null 转成 1， 0， 0；parseInt（）转成 NaN
+ */
---
+++ byJavaScript/codewars/8kyu/004_sum the strings.js
@@ -0,0 +1,18 @@
+/*
+Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum
+(also as a string):
+
+sumStr("4", "5")    // should output "9"
+sumStr("34", "5")   // should output "39"
+
+If either input is an empty string, consider it as zero.
+
+ */
+
+function sumStr(a, b) {
+    let sum = a * 1 + b * 1;
+    let str = sum.toString();
+    return str;
+}
+
+console.log(sumStr('', '2'));
